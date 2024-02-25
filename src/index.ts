fce6afdafe4fd74519a42feba8bac417f8b4e395
thadeu <guilherme.thadeu@qodeless.io>
2024-02-25 16:03:09 -0300

    first commit bun elysia

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia")
+.get('/post/:id', ({params: {id}})=> {return {id: id, title: "Learn Bun"}})
+.post('/post', ({body}) => {return body})
+.get('/track/*', () => {return 'Track Route'}) 
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
