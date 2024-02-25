5658cd3e75148067b3a188633cc0865ae8bb5b74
thadeu <guilherme.thadeu@qodeless.io>
2024-02-25 16:33:07 -0300

    route groups

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,69 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { plugin } from "./plugin";
 
+
+//APPLICATION
 const app = new Elysia().get("/", () => "Hello Elysia")
+.use(plugin)
+.state({
+  id: 1,
+  email: 'thadeu@gmail.com'
+})
+.decorate('getDate', () => Date.now())
 .get('/post/:id', ({params: {id}})=> {return {id: id, title: "Learn Bun"}})
-.post('/post', ({body}) => {return body})
+.post('/post', ({body, set, store}) => { 
+  console.log(store)
+  set.status = 201
+  return body
+})
 .get('/track/*', () => {return 'Track Route'}) 
-.listen(3000);
+.get('/tracks', ({store, getDate}) => {
+  // return new Response(JSON.stringify({
+  //   "tracks": [
+  //     'Dancing Feat',
+  //     'Sam I',
+  //     'Animals'
+  //   ]
+  // }), {
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  // })
+  console.log(store)
+  console.log(getDate())
+  console.log(store['plugin-version'])
+  return {
+    "tracks": [
+      'Dancing Feat',
+      'Sam I',
+      'Animals',
+      'New song'
+    ]
+  }
+});
+
+
+app.group('/user', app => app
+.post('/sign-in', ({body}) => body, {
+  body: t.Object({
+    username: t.String(),
+    password: t.String()
+  })
+})
+.post('/sign-up', () => "Signup Route")
+.post('/profile', () => "Profile Route")
+.get('/:id', () => "User by id Route"))
+
+
+app.group('/v1', app => app
+.get('/', () => "Version 1")
+.group('/products', app => app
+.post('/', () => "Create Product")
+.get('/:id', () => "GET PRODUCT BY ID")
+.put('/:id', () => "UPDATE PRODUCT BY ID")
+.delete('/:id', () => "DELETE PRODUCT BY ID")))
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
